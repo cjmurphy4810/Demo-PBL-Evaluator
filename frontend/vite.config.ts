854ed5399bb8416481e5baa965a0edfca5e7e340
cjmurphy4810 <chrisjmurphyauthor@gmail.com>
2026-04-24 07:58:08 -0700

    Change base path in Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/PBL-Evaluator/",
+  base: "/Demo-PBL-Evaluator/",
   plugins: [react()],
   resolve: {
     alias: {
